9edb895eefc5813a2994976efc66576fb2ee65a4
WMXPY <wm@mengw.io>
2019-01-15 14:03:09 -0600

    implement sandbox

--- src/call.ts
+++ src/call.ts
@@ -1,16 +1,31 @@
 /**
  * @author WMXPY
- * @namespace Mock_Sandbox
+ * @namespace Sudoo_Mock
  * @description Call
  */
 
 export class Call {
 
-    private _args: any[];
+    public static create(args: any[]): Call {
 
-    public constructor(args: any[]) {
+        return new Call(args);
+    }
+
+    private readonly _args: any[];
+
+    private constructor(args: any[]) {
 
         this._args = args;
+    }
+
+    public get length(): number {
+
+        return this._args.length;
+    }
+
+    public get args(): any[] {
+
+        return this._args;
     }
 
     public arg(order: number) {

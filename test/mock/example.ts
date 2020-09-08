c644f57c63b8edd8b89890bd81ca885e03ae4c3f
WMXPY <wm@mengw.io>
2020-09-08 12:13:23 -0500

    add mock file

---
+++ test/mock/example.ts
@@ -0,0 +1,11 @@
+/**
+ * @author WMXPY
+ * @namespace Mock
+ * @description Example
+ * @package Mock
+ */
+
+export const functionThatReturnOne = (): number => {
+
+    return 1;
+};
--- test/unit/mock.test.ts
+++ test/unit/mock.test.ts
@@ -9,7 +9,7 @@ import { expect } from 'chai';
 
 describe('Given {Mock} Class', (): void => {
 
-    it('p', (): void => {
+    it('should be able to mock function', (): void => {
 
         expect(1).to.be.equal(1);
     });
